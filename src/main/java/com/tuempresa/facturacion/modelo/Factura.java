c547dd782f5f6171d30807108f306dfc89bfbae8
adragutg400 <adragut@gestion400.com>
2024-10-08 11:56:47 +0200

    Created class Factura with attributes that uses default values and a custom calculator class

---
+++ src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -0,0 +1,40 @@
+package com.tuempresa.facturacion.modelo;
+
+import java.time.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.*;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+
+import com.tuempresa.facturacion.calculadores.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Factura {
+
+	@Id
+	@Hidden
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid")
+	@Column(length=32)
+	String oid;
+	
+	@Column(length=4)
+	@DefaultValueCalculator(CurrentYearCalculator.class)
+	int anyo;
+	
+	@Column(length=6)
+	@DefaultValueCalculator(value=CalculadorSiguienteNumeroParaAnyo.class, properties=@PropertyValue(name="anyo"))
+	int numero;
+	
+	@Required
+	@DefaultValueCalculator(CurrentLocalDateCalculator.class)
+	LocalDate fecha;
+	
+	@TextArea
+	String observaciones;
+}
