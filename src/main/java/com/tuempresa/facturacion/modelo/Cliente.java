141d4ea2c9e3201bd82fb3171a215126fe749737
adragutg400 <adragut@gestion400.com>
2024-10-08 10:16:16 +0200

    Created Cliente class

---
+++ src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
@@ -0,0 +1,21 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+	
+	@Id
+	@Column(length=6)
+	int numero;
+	
+	@Column(length=60)
+	@Required
+	String nombre;
+
+}
