6742d3b6db4ba0a48a23f58f7743e1da3b1845e2
adragutg400 <adragut@gestion400.com>
2024-10-08 11:24:50 +0200

    Added other attributes using OpenXava annotations in Producto class

--- src/main/java/com/tuempresa/facturacion/modelo/Producto.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -1,5 +1,7 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.math.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
@@ -15,5 +17,20 @@ public class Producto {
 	@Column(length=50)
 	@Required
 	String descripcion;
+	
+	@ManyToOne(fetch=FetchType.LAZY, optional=true)
+	@DescriptionsList
+	Categoria categoria;
+	
+	@Money
+	BigDecimal precio;
+	
+	@Files
+	@Column(length=32)
+	String fotos;
+	
+	@TextArea
+	String observaciones;
+	
 
 }
