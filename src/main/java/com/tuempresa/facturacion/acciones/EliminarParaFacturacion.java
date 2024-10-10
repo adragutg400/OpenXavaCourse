4d83769791f4130f2f7b9c89c231f145342029f3
adragutg400 <adragut@gestion400.com>
2024-10-10 12:45:22 +0200

    Changed controller and action to generic

---
+++ src/main/java/com/tuempresa/facturacion/acciones/EliminarParaFacturacion.java
@@ -0,0 +1,26 @@
+package com.tuempresa.facturacion.acciones;
+
+import java.util.*;
+
+import org.openxava.actions.*;
+import org.openxava.model.*;
+
+public class EliminarParaFacturacion extends ViewBaseAction{
+
+	@Override
+	public void execute() throws Exception {
+		Map<String, Object> valores =
+				new HashMap<>();
+		valores.put("eliminado", true);
+		MapFacade.setValues(
+				getModelName(), 
+				getView().getKeyValues(),
+				valores);
+		resetDescriptionsCache();
+		addMessage(
+			"object_deleted", getModelName());
+		getView().clear();
+		getView().setKeyEditable(true);
+		getView().setEditable(false);
+	}
+}
